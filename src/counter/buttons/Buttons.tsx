5114cb50d118e56b6423b45f9f4679748d8510bc
lya-melnik <ilay.melnik.kr@gmail.com>
2023-01-09 23:27:10 -0800

    Counter_1 (update) - refactoring

--- src/counter/buttons/Buttons.tsx
+++ src/counter/buttons/Buttons.tsx
@@ -9,13 +9,13 @@ type Buttons = {
     inc: number
 }
 const Buttons = (props: Buttons) => {
-   const incBtnStyle = props.inc === 5 ? s.disabled : s.ink
-   const resBtnStyle = props.inc === 0  ? s.disabled : s.ink
+    const incBtnStyle = props.inc === 5 ? s.disabled : s.ink
+    const resBtnStyle = props.inc === 0 ? s.disabled : s.ink
 
     return (
         <div className={s.btns}>
-            <Button name="inc"  disabled={props.inc === 5} callback={props.IncrementHandler} className={incBtnStyle}/>
-            <Button name="reset"  disabled={props.inc === 0} callback={props.ResetHandler} className={resBtnStyle} />
+            <Button name="inc" disabled={props.inc === 5} callback={props.IncrementHandler} className={incBtnStyle}/>
+            <Button name="reset" disabled={props.inc === 0} callback={props.ResetHandler} className={resBtnStyle}/>
         </div>
     );
 };
